305638345e8bbec533a8ff0acccf7c46750b8b81
Teagan Stewart <teaganstewart@gmail.om>
2020-12-14 20:40:13 +1300

    Part 1 - Day 4
    Created the read file method, starting with a normal method, then I used new lamda expressions and querys i learnt instead.

--- day4/PassportProcessor.cs
+++ day4/PassportProcessor.cs
@@ -1,45 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Text.RegularExpressions;
+using static System.Text.RegularExpressions.RegexOptions;
+using System.Linq;
 
 namespace AdventOfCode
 {
     /*
-    * Checks the path to see the number of trees in the way. Day 3 of the Advent of Code challenge.
     *
-    * The main idea is to check the path the Toboggan will travel down for trees. The toboggan can only move 3 to the right and 1 to the left.
     *
-    * ..##.......
-    * #..O#...#.. is an example field, where #'s represent trees and dots are clear paths.
     */
     class PassportProcessor
     {
+        
 
-        List<int> lines;
-
-        public void ReadInputFile()
+        public void CheckPassports()
         {
-            // initalisation of storage variables
-            int counter = 0;
+
             string line;
-            lines = new string[323];
+            string passport = "";
+            StreamReader file = new StreamReader(@"input.txt");
+            List<string> seperatedPassports = new List<string>();
 
-            // Read the file and display it line by line.  
-            System.IO.StreamReader file = new System.IO.StreamReader(@"input.txt");
+            while((line = file.ReadLine()) != null) {
+                if(line == "") {
+                    seperatedPassports.Add(passport);
+                    passport = "";
 
-            while ((line = file.ReadLine()) != null)
-            {
-                lines[counter] = line;
-                counter++;
+                }
+                else {
+                    passport += line + " ";
+                }  
             }
 
-            file.Close();
-            Console.WriteLine("There were {0} lines read.", counter);
+            seperatedPassports.Add(passport);
 
+            // creates a list of dictionaries, containing the fields of each passport
+            var passports = seperatedPassports
+                .Select(passport => Regex.Split(passport, @"\s+")
+                .Where(field => !string.IsNullOrEmpty(field))
+                .Select(field =>
+                {
+                    string[] pairValues = field.Split(":");
+                    return new KeyValuePair<string, string>(pairValues[0], pairValues[1]);
+
+                }).ToImmutableDictionary()
+
+                ).ToImmutableList();
+
+                var requiredFields = new[] { "ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt" }.ToImmutableHashSet();
+                Console.WriteLine("There are {0} valid passports.", passports.Count(passport => requiredFields.IsSubsetOf(passport.Keys)));
         }
 
         static void Main(string[] args)
         {
+
             PassportProcessor p = new PassportProcessor();
+            p.CheckPassports();
 
         }
     }
