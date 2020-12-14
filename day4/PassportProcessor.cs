471691a58fe1e9edeef7e23bf20975f0926adb50
Teagan Stewart <teaganstewart@gmail.om>
2020-12-14 21:09:40 +1300

    Added Helper Method Templates

--- day4/PassportProcessor.cs
+++ day4/PassportProcessor.cs
@@ -14,28 +14,35 @@ namespace AdventOfCode
     */
     class PassportProcessor
     {
-        
 
-        public void CheckPassports()
+        List<string> seperatedPassports;
+
+        public void ReadInputFile()
         {
-
             string line;
             string passport = "";
             StreamReader file = new StreamReader(@"input.txt");
-            List<string> seperatedPassports = new List<string>();
+            seperatedPassports = new List<string>();
 
-            while((line = file.ReadLine()) != null) {
-                if(line == "") {
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line == "")
+                {
                     seperatedPassports.Add(passport);
                     passport = "";
 
                 }
-                else {
+                else
+                {
                     passport += line + " ";
-                }  
+                }
             }
 
             seperatedPassports.Add(passport);
+        }
+
+        public void CheckPassports()
+        {
 
             // creates a list of dictionaries, containing the fields of each passport
             var passports = seperatedPassports
@@ -50,14 +57,51 @@ namespace AdventOfCode
 
                 ).ToImmutableList();
 
-                var requiredFields = new[] { "ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt" }.ToImmutableHashSet();
-                Console.WriteLine("There are {0} valid passports.", passports.Count(passport => requiredFields.IsSubsetOf(passport.Keys)));
+            var requiredFields = new[] { "ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt" }.ToImmutableHashSet();
+            Console.WriteLine("There are {0} valid passports.", passports.Count(passport => requiredFields.IsSubsetOf(passport.Keys)));
+        }
+
+        // ---------------------------------------
+        // ---------- HELPER METHODS -------------
+        // ---------------------------------------
+
+        bool CheckBirthYear() {
+
+        }
+
+        bool CheckIssueYear() {
+
+        }
+
+        bool CheckExpirationDate() {
+
+        }
+
+        bool CheckHeight() {
+
+        }
+
+        bool CheckHairColor() {
+
+        }
+
+        bool CheckEyeColor() {
+
+        }
+
+        bool CheckPasswordId() {
+
+        }
+
+        bool CheckCountryId() {
+            
         }
 
         static void Main(string[] args)
         {
 
             PassportProcessor p = new PassportProcessor();
+            p.ReadInputFile();
             p.CheckPassports();
 
         }
