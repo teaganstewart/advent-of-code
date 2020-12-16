43146f112cd22d966df47bb9651bc338d955d55f
Teagan Stewart <teaganstewart@gmail.om>
2020-12-16 22:41:57 +1300

    Setup
    Setup input and files of days 7 to 10.

---
+++ day10/AdapterArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// </summary>
+    class CustomCustoms
+    {
+        static void Main(string[] args)
+        {
+            CustomCustoms c = new CustomCustoms();
+            c.ReadInputFile();
+            c.ProcessYesAnswers();
+            c.ProcessIntersectAnswer();
+        }
+    }
+}
