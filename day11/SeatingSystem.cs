6f91e413cc73ed96d54e8f5790186fac81e65f9e
Teagan Stewart <teaganstewart@gmail.om>
2020-12-20 19:40:50 +1300

    Added day 11

---
+++ day11/SeatingSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    /// <summary>
+    /// </summary>
+    class SeatingSystem
+    {
+
+        void ReadInputFile()
+        {
+            
+        }
+
+        static void Main(string[] args)
+        {
+            SeatingSystem s = new SeatingSystem();
+            s.ReadInputFile();
+        }
+    }
+}
